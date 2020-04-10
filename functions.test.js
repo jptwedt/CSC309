f8a7192b0e138fc8c7781963d4a23a74e2b26f3f
jtwedt <27270276+jptwedt@users.noreply.github.com>
2020-04-10 13:08:17 -0700

    first step: write a failing test

--- functions.test.js
+++ functions.test.js
@@ -4,3 +4,8 @@ test('should return Hello, <name>.', () => {
    const answer = fxn.greet('Bob');
    expect(answer).toMatch(/Hello, Bob\./);
 });
+
+test('passing null, should return Hell, my friend.', () => {
+   const answer = fxn.greet();
+   expect(answer).toMatch(/Hello, my friend\./);
+});
