3e3ef51298bd06aa26fedac2995db286d0c0f317
jtwedt <27270276+jptwedt@users.noreply.github.com>
2020-04-10 21:39:39 -0700

    req 7 done, refactored and added more tests

--- functions.js
+++ functions.js
@@ -1,65 +1,116 @@
 var fxn = {
-   greet: function(...names){
+   greet: function(...args){
       let def = 'my friend';
       let resp = 'Hello, ';
       let RESP = ' AND HELLO ';
 
-      if(names.length == 0){
+      if(args.length == 0){
          resp += (def + '.');
       }
-      else if(names.length == 1){
-         let NAME = names[0].toUpperCase();
-         if(names[0] == NAME){
-            resp = resp.toUpperCase() + NAME + '!';
-         }
-         else{
-            resp += (names[0] + '.');
-         }
-      }
       else{
-         let lower = [];
-         let upper = [];
-         names.forEach(name => {
-            if(name == name.toUpperCase()){
-               upper.push(name);
+         let argarr = Array.prototype.slice.call(args);
+         let names = new Array();
+         argarr.forEach(arg => {
+            if(arg.includes(", ")){
+               ppl = arg.split(", ");
+               ppl.forEach(person => {
+                  names.push(person);
+               });
             }
             else{
-               lower.push(name);
+               names.push(arg);
             }
          });
-         if(lower.length == 0){
+
+         if(module.exports.isAllUpper(...names)){
             resp = resp.toUpperCase();
-            resp += upper[0];
-            upper.slice(1).forEach(up => {
-               resp += (' AND ' + up);
-            });
+            names.reverse();
+            resp += names.pop();
+            if(names.length > 0){
+               let len = names.length;
+               while(names.length > 1){
+                  resp += (', ' + names.pop());
+               }
+               resp = (len > 1)?
+                  resp + ', AND ' + names.pop():
+                  resp + ' AND ' + names.pop();
+            }
             resp += '!';
          }
-         else if(upper.length == 0){
-            resp += lower[0];
-            lower.slice(1).forEach(low => {
-               resp += (' and ' + low);
-            });
+         else if(module.exports.allHasLower(...names)){
+            names.reverse();
+            resp += names.pop();
+            if(names.length > 0){
+               let len = names.length;
+               while(names.length > 1){
+                  resp += (', ' + names.pop());
+               }
+               resp = (len > 1)?
+                  resp + ', and ' + names.pop():
+                  resp + ' and ' + names.pop();
+            }
             resp += '.';
          }
          else{
-            resp += lower[0];
-            if(lower.length > 1){
-               lower.slice(1).forEach(low => {
-                  resp += (' and ' + low);
-               });
+            let lower = [];
+            let upper = [];
+            names.forEach(name => {
+               if(module.exports.hasLower(name)){
+                  lower.push(name);
+               }
+               else{
+                  upper.push(name);
+               }
+            });
+            lower.reverse();
+            upper.reverse();
+            resp += lower.pop();
+            if(lower.length > 0){
+               let len = lower.length;
+               while(lower.length > 1){
+                  resp += (', ' + lower.pop());
+               }
+               resp = (len > 1)?
+                  resp + ', and ' + lower.pop():
+                  resp + ' and ' + lower.pop();
             }
             resp += '.';
-            resp += RESP + upper[0];
-            if(upper.length > 1){
-               upper.slice(1).forEach(up => {
-                  resp += (' AND ' + up);
-               });
+            resp += RESP + upper.pop();
+            if(upper.length > 0){
+               let len = upper.length;
+               while(upper.length > 1){
+                  resp += (', ' + upper.pop());
+               }
+               resp = (len > 1)?
+                  resp + ', and ' + upper.pop():
+                  resp + ' and ' + upper.pop();
             }
             resp += '!';
          }
       }
       return resp;
+   },
+   allHasLower: function(...names){
+      var all_lower = true;
+      names.forEach(name => {
+         all_lower = (all_lower && module.exports.hasLower(name));
+      });
+      return all_lower;
+   },
+   isAllUpper: function(...names){
+      var all_upper = true;
+      names.forEach(name => {
+         all_upper = (all_upper && module.exports.isUpper(name));
+      });
+      return all_upper;
+   },
+   hasLower: function(inname){
+      name = String(inname);
+      return name != name.toUpperCase();
+   },
+   isUpper: function(inname){
+      name = String(inname);
+      return name == name.toUpperCase();
    }
 }
 
--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,5 @@
 const fxn = require('./functions.js');
 
-
 test('should return Hello, <name>.', () => {
    const answer = fxn.greet('Bob');
    expect(answer).toMatch(/Hello, Bob\./);
@@ -29,5 +28,65 @@ test('passing mixed args, two lower, one upper', () => {
 test('passing irregular grouping, (lower), (lower,lower)', () => {
    const answer = fxn.greet("Bob", "Charlie, Dianne");
    expect(answer).toMatch(/Hello, Bob, Charlie, and Dianne\./);
+});
+
+test('testing uppercase detection, expect true', () => {
+   const answer = fxn.isUpper("BOB");
+   expect(answer).toBeTruthy();
+});
+
+test('testing uppercase detection, expect false', () => {
+   const answer = fxn.isUpper("Bob");
+   expect(answer).not.toBeTruthy();
+});
+
+test('testing lowercase detection, expect true', () => {
+   const answer = fxn.hasLower("Bob");
+   expect(answer).toBeTruthy();
+});
+
+test('testing lowercase detection, expect false', () => {
+   const answer = fxn.hasLower("BOB");
+   expect(answer).not.toBeTruthy();
+});
+
+test('testing all uppercase detection, expect true', () => {
+   const answer = fxn.isAllUpper("BOB", "TIMOTHY");
+   expect(answer).toBeTruthy();
+});
+
+test('testing all uppercase detection, expect false', () => {
+   const answer = fxn.isAllUpper("Bob", "timothy");
+   expect(answer).not.toBeTruthy();
+});
+
+test('testing all uppercase detection with mixed expect false', () => {
+   const answer = fxn.isAllUpper("Bob", "TIMOTHY");
+   expect(answer).not.toBeTruthy();
+});
+
+test('testing all uppercase detection with mixed expect false', () => {
+   const answer = fxn.isAllUpper("Bob", "TIMOTHY", "gina");
+   expect(answer).not.toBeTruthy();
+});
+
+test('testing all lowercase detection, expect true', () => {
+   const answer = fxn.allHasLower("Bob", "martha");
+   expect(answer).toBeTruthy();
+});
+
+test('testing lowercase detection, expect false', () => {
+   const answer = fxn.allHasLower("BOB", "MARTHA");
+   expect(answer).not.toBeTruthy();
+});
+
+test('testing lowercase detection with mixed, expect false', () => {
+   const answer = fxn.allHasLower("bob", "MARTHA");
+   expect(answer).not.toBeTruthy();
+});
+
+test('testing lowercase detection with 3 mixed, expect false', () => {
+   const answer = fxn.allHasLower("bob", "MARTHA", "NED");
+   expect(answer).not.toBeTruthy();
 });
 
